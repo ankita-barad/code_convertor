b75713045f924f083af697d12110e158dfa16855
ankita-barad <ankitabarad2111@gmail.com>
2023-09-13 21:50:24 +0530

    updated the frontend code

--- backend/index.js
+++ backend/index.js
@@ -1,64 +1,65 @@
 require("dotenv").config();
+const OpenAI = require("openai");
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
 
-const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/convert", async (req, res) => {
-  const { sourceCode, targetLanguage } = req.body;
+app.post("/api/convert", async (req, res) => {
+  const { code, targetLanguage } = req.body;
+
+  // Construct the prompt for GPT-3.5
+  const prompt = `Please convert the following code from ${code} to ${targetLanguage},
+   you response should be a json string which I can parse with JSON.parse having code as key
+  `;
 
   try {
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/text-davinci-002/completions",
-      {
-        prompt: `Translate the following code from ${targetLanguage} to ${targetLanguage}:\n${sourceCode}`,
-        max_tokens: 150,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${OPENAI_API_KEY}`,
-        },
-      }
-    );
-
-    const translatedCode = response.data.choices[0].text;
-    res.status(200).json({ translatedCode });
+    // Call the GPT-3.5 API to convert code
+    const openaiResponse = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      // max_tokens: 50, // Adjust as needed
+    });
+    res.status(200).send(JSON.parse(openaiResponse.choices[0].message.content));
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Conversion failed" });
+    res.status(400).send({
+      isError: true,
+      message: error,
+    });
   }
 });
+
 app.post("/api/debug", async (req, res) => {
+  const sourceCode = req.body.sourceCode;
+
+  // Construct the prompt for GPT-3.5
+  const prompt = `Debug the following code: \n${sourceCode} \n explain what wrong and give me a solution to it, 
+  your response should be a json string which I can parse with explaination as one key and the code as another key
+  `;
+
   try {
-    const sourceCode = req.body.sourceCode;
-
-    // Send the source code to the OpenAI API for code generation and explanation
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/text-davinci-002/completions",
-      {
-        prompt: `Debug the following code:\n${sourceCode} return updated source code and explaination to fix the issue in code`,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${OPENAI_API_KEY}`,
-        },
-      }
-    );
-
-    // Extract the explanation and corrected code from the OpenAI response
-    const correctedCodeWithExplanation = response.data.choices[0].text;
-
-    // Respond with the explanation and the corrected code
-    res.status(200).json({ correctedCodeWithExplanation });
+    // Call the GPT-3.5 API to convert code
+    const openaiResponse = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      // max_tokens: 50, // Adjust as needed
+    });
+    res.status(200).send(JSON.parse(openaiResponse.choices[0].message.content));
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Debugging and explanation failed" });
+    res.status(400).send({
+      isError: true,
+      message: error,
+    });
   }
 });
 
@@ -66,31 +67,36 @@ app.post("/api/code-quality", async (req, res) => {
   try {
     const sourceCode = req.body.sourceCode;
 
-    // Send the source code to the OpenAI API for code quality analysis
-    const response = await axios.post(
-      "https://api.openai.com/v1/engines/text-davinci-002/completions",
-      {
-        prompt: `Analyze the code quality based on the following parameters:\n
-          1. Consistency in Coding Style\n
-          2. Comments and Documentation\n
-          3. Code Structure and Modularity\n
-          4. Error Handling\n
-          5. Code Duplications and Code Smells\n
-          Code:\n${sourceCode}`,
-        max_tokens: 150,
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${OPENAI_API_KEY}`,
-        },
-      }
-    );
+    const prompt = `Analyze the code quality based on the following parameters:\n
+    1. Consistency in Coding Style\n
+    2. Comments and Documentation\n
+    3. Code Structure and Modularity\n
+    4. Error Handling\n
+    5. Code Duplications and Code Smells\n
+    Code:\n${sourceCode}\n 
+    give the response as json object in this format strictly,
+    {
+        "explaination" : "Consistency in Coding Style\n
+        answer\n
+        Comments and Documentation\n
+        answer\n
+        Code Structure and Modularity\n
+        answer\n
+        Error Handling\n
+        answer\n
+        Code Duplications and Code Smells\n
+        answer\n"
+    }
 
-    // Extract the code quality analysis from the OpenAI response
-    const codeQualityAnalysis = response.data.choices[0].text;
+    and replace the answer with explaination for each parameter, don't forget the newline character
+    `;
 
-    // Respond with the code quality analysis
-    res.status(200).json({ codeQualityAnalysis });
+    const openaiResponse = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [{ role: "user", content: prompt }],
+      // max_tokens: 50, // Adjust as needed
+    });
+    res.status(200).send(JSON.parse(openaiResponse.choices[0].message.content));
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Code quality analysis failed" });
